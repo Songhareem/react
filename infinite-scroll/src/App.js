3b38e460d11b2e07be70af6aec7c8b925d4a1fe4
Songhareem <elaha00@naver.com>
2021-06-16 09:51:36 +0900

    update scroll tutorial / start academy

--- infinite-scroll/src/App.js
+++ infinite-scroll/src/App.js
@@ -4,8 +4,8 @@ import VirtualScroller from "./Components/VirtualScroller";
 function App() {
   return (
     <div className="App">
-      <h1>Virtual Scroll</h1>
-      <h2>Let's see How this works!</h2>
+      {/* <h1>Virtual Scroll</h1>
+      <h2>Let's see How this works!</h2> */}
       <VirtualScroller />
     </div>
   );
